8df174b156ce968c9f41df6b7b2214355d1ff0b6
spapp <stefan.papp@dxc.com>
2018-12-21 18:07:29 +0400

    first version that reads out full files and pushes into topics

--- readme.md
+++ readme.md
@@ -2,3 +2,17 @@
 
 We have csv file that has meta data and meta data. We want to find a way to
 automatically load metadata and data into different topics.
+
+To start, we use Apache Kafka
+
+#start zookeeper
+bin/zookeeper-server-start.sh config/zookeeper.properties
+
+#start kafka
+bin/kafka-server-start.sh config/server.properties
+ 
+#create topic 
+bin/kafka-topics.sh --create --topic kmi_test --zookeeper localhost:2181 --replication-factor 1 --partitions 1
+
+#create consumer 
+kafka-console-consumer.sh --bootstrap-server=localhost:9092 --consumer.config=config/consumer.properties --topic kmi_test --from-beginning
--- src/datawrangler/__main__.py
+++ src/datawrangler/__main__.py
@@ -25,15 +25,18 @@ def _main():
     with open(args.config, 'r') as yaml_file:
         config = yaml.load(yaml_file)
 
-    host = config.get('broker', 'host')
+    host = config['broker']['host']
+    port = config['broker']['port']
+    topic = config['files']['topic']
+    dir = config['files']['dir']
 
-    broker = "{}:{}".format(config.get('broker', 'host'), config.get('broker', 'port'))
+    broker = "{}:{}".format(host, port)
 
     client = KafkaClient(hosts=broker)
-    topic = client.topics[config.get('kafka', 'topic').encode()]
-    for file in os.listdir(client.topics[config.get('files', 'dir')]):
+    topic = client.topics[topic.encode()]
+    for file in os.listdir(dir):
         with topic.get_sync_producer() as producer:
-            with open(file) as fp:
+            with open(os.path.join(dir, file)) as fp:
                 lines = fp.readlines()
                 for line in lines:
                     producer.produce(line.encode())
